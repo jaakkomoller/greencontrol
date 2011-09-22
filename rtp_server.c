c11bde337cedea798be097624525f6b69a7c52a5
Jaakko Korkeaniemi <jkorkean@gmail.com>
2011-09-23 01:22:38 +0300

    RTP-server stub file
    Currently only reads the example file trough.

---
+++ rtp_server.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() {
+	FILE *infile;
+	char fname[] = "test_data/original.au";
+	char line[100];
+	int readchars = 0;
+ 
+	/* Open the file.  If NULL is returned there was an error */
+	if((infile = fopen(fname, "r")) == NULL) {
+		printf("Error Opening File.\n");
+		exit(1);
+	}
+  
+	readchars = fread (line, 1, 100, infile);
+	while(readchars != 0) {
+		printf("read line with %d chars\n", readchars);  
+		readchars = fread (line, 1, 100, infile);
+	}
+ 
+	fclose(infile);  /* Close the file */
+}
+
+

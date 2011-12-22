2d307cdd1e450c553050ba502d8cf291c6b63280
Aimmy <aimveeny@gmail.com>
2011-12-22 21:44:45 +0200

    Time printing

---
+++ timing.c
@@ -0,0 +1,28 @@
+/*
+ *  time.c
+ *  
+ *
+ *  Created by Sarantorn Bisalbutra on 12/21/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "timing.h"
+
+
+void print_time(const char* message)
+{
+	char buffer[30];
+	struct timeval tv;
+	
+	time_t curtime;
+	
+	
+	gettimeofday(&tv, NULL); 
+	curtime=tv.tv_sec;
+	
+	strftime(buffer,30,"%m-%d-%Y  %T.",localtime(&curtime));
+	printf("%s:%s%ld\n",message,buffer,tv.tv_usec);
+	
+	
+}

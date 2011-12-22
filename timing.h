ba8a70b21985555a2fb44de349f272dc579fdd53
Aimmy <aimveeny@gmail.com>
2011-12-22 21:44:55 +0200

    Time printing

---
+++ timing.h
@@ -0,0 +1,17 @@
+/*
+ *  time.h
+ *  
+ *
+ *  Created by Sarantorn Bisalbutra on 12/21/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+
+#include <time.h> /*TIME*/
+#include <stdio.h> /* for FILE streams, input/output*/
+#include <strings.h> /* for bzero */
+#include <stdlib.h>
+
+
+void print_time(const char* message);
